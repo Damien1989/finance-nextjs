acbbf05c2b1dc58ec5265cf40e534f2dc65fbadd
damien <contact.damiendiaz@gmail.com>
2025-01-01 12:14:51 +0100

    creation budget part 1

--- app/actions.ts
+++ app/actions.ts
@@ -23,5 +23,28 @@ export async function checkAndAddUser(email: string | undefined) {
     } catch (error) {
         console.error("Erreur lors de la vérification de l'utilisateur:", error);
     }
-
 }
+    export async function addBudget(email : string, name:string, amount:number, emoji: string) {
+            try {
+                    const user = await prisma.user.findUnique({
+                        where: { email }
+                    })
+
+                    if (!user) {
+                        throw new Error ('Utilisateur non trouvé')
+                    }
+
+                    await prisma.budget.create({
+                        data: {
+                            name,
+                            amount,
+                            emoji,
+                            userId : user.id
+                        }
+
+                    })
+            } catch (error){
+                console.error("erreur budget", error);
+                throw error
+            }
+    }
--- app/budgets/page.tsx
+++ app/budgets/page.tsx
@@ -1,10 +1,15 @@
 "use client"
 
-import React, { useEffect} from 'react'
+import React, { useEffect, useState} from 'react'
 import Navbar from '../components/Navbar'
 import Wrapper from '../components/Wrapper'
+import { useUser } from '@clerk/nextjs'
 
 const page = () => {
+
+    const { user } = useUser()
+    const [budgetName, setBudgetName] = useState<string>("")
+    const [budgetAmount, setBudgetAmount] = useState<string>("")
 
   return (
 <Wrapper>Hello</Wrapper>
